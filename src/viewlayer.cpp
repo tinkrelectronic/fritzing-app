390943f3b00fdb935dfc5abf5e613803702fe7b1
Kjell Morgenstern <kjell@fritzing.org>
2024-02-12 17:58:58 +0100

    Remove dependency

--- src/viewlayer.cpp
+++ src/viewlayer.cpp
@@ -19,7 +19,7 @@ along with Fritzing.  If not, see <http://www.gnu.org/licenses/>.
 ********************************************************************/
 
 #include "viewlayer.h"
-#include "debugdialog.h"
+#include "utils/misc.h"
 #include "utils/misc.h"
 #include <qmath.h>
 
