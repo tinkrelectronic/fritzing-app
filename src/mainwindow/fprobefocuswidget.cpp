77ffe3d31ce415c808a5b0147fab67b4b28d9189
Kjell Morgenstern <support@fritzing.org>
2024-06-07 11:48:40 +0200

    Updated probe so that an index is not mandatory.

--- src/mainwindow/fprobefocuswidget.cpp
+++ src/mainwindow/fprobefocuswidget.cpp
@@ -19,5 +19,9 @@ void FProbeFocusWidget::write(QVariant data)
 		QString objectName = parts[0];
 		int index = parts[1].toInt();
 		emit focusWidget(objectName, index);
+	} else if (parts.size() == 1) {
+		QString objectName = parts[0];
+		int index = 0;
+		emit focusWidget(objectName, index);
 	}
 }
