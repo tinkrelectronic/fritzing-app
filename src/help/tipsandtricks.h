8e7887628bbedc7d3237bb1c9a5c5bcb6ef04127
TinkR Electornic <tinkerelectronic@gmail.com>
2025-01-01 18:56:41 -0800

    Update tipsandtricks.h

--- src/help/tipsandtricks.h
+++ src/help/tipsandtricks.h
@@ -27,7 +27,7 @@ along with Fritzing.  If not, see <http://www.gnu.org/licenses/>.
 #include <QTextEdit>
 #include <QFile>
 #include <QList>
-#include <QtextBrowser>
+#include <QTextBrowser>
 
 #include "../utils/misc.h"
 
